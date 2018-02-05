2ff898e7fb511174c97915e31c3b3c6901f74933
Akshit Sahani <akshit_sahani@hotmail.com>
2018-02-05 16:50:03 +0530

    search results container and buttons overlaid onto map

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,17 +1,19 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Actions} from 'react-native-router-flux';
-import {Card, Button, Header} from './common';
+import {Card, CardItem, Button, Header} from './common';
 import Map from './Map';
 
 class SearchResults extends Component {
   render(){
     return(
       <Card>
-        <Map height="80%" width="100%" />
-        <Header title="Search Results"/>
-        <Button content="book" pressed={() => {Actions.LoginForm();}}/>
-        <Button content="Contact" pressed={() => {Actions.LoginForm();}}/>
+        <Map height="100%" width="100%" />
+        <CardItem style={{position: 'absolute', bottom:'30%', left: '30%', zIndex: 1}}>
+          <Header title="Search Results"/>
+          <Button content="book" pressed={() => {Actions.LoginForm();}}/>
+          <Button content="Contact" pressed={() => {Actions.LoginForm();}}/>
+        </CardItem>
       </Card>
     )
   };
