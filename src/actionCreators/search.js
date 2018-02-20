0e7ab58c58e6b61fca71f108c72e3618a6577923
Feida <feidameng01@gmail.com>
2018-02-20 13:55:00 -0500

    debug date format

--- src/actionCreators/search.js
+++ src/actionCreators/search.js
@@ -23,7 +23,7 @@ const isWithinMyRadius = (myLocation, userLocation, radius) => {
 
   const a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(myLocation.latitude) * Math.cos(userLocation.latitude);
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-  console.log(`${earthRadiusKm * c} <= ${radius} ? `,earthRadiusKm * c <= radius)
+  // console.log(`${earthRadiusKm * c} <= ${radius} ? `,earthRadiusKm * c <= radius)
   const distance = earthRadiusKm * c;
   return [distance <= radius, distance];
 }
@@ -94,20 +94,34 @@ export const search = inputData => {
                 };
                 break;
             case "availabilityMin":
+                const dateMin = inputData[prop].split("-");
+                console.log('dateMin: ', dateMin);
+                const newDateMin = dateMin[0]+"/"+dateMin[1]+"/"+dateMin[2];
+                console.log('newDateMin: ', newDateMin);
+                const timestampMin = new Date(newDateMin).getTime();
+                console.log('timestampMin: ', timestampMin);
                 filter[prop] = {
-                    "operator": ">=",
-                    "value": inputData[prop]
+                    "operator": "<=",
+                    "value": timestampMin
                 };
                 break;
             case "availabilityMax":
+                const dateMax = inputData[prop].split("-");
+                console.log('dateMax: ', dateMax);
+                const newDateMax = dateMax[0]+"/"+dateMax[1]+"/"+dateMax[2];
+                console.log('datenewDateMax: ', newDateMax);
+                const timestampMax = new Date(newDateMax).getTime();
+                console.log('timestampMax: ', timestampMax);
+
                 filter[prop] = {
-                    "operator": "<=",
-                    "value": inputData[prop]
+                    "operator": ">=",
+                    "value": timestampMax
                 };
                 break;
           }
         }
       }
+
 
       console.log('queryJson: ', queryJson);
       console.log('filter: ', filter);
@@ -149,12 +163,23 @@ export const search = inputData => {
             //   console.log(`${user[prop]} ${filter[prop].operator} ${filter[prop].value}`,eval(`${user[prop]} ${filter[prop].operator} ${filter[prop].value}`));
             // }
 
+            if (prop === 'availabilityMax') {
+              console.log(`${user.name}: (${prop}: ${Date.parse(user[prop])}) ${filter[prop].operator} (input: ${filter[prop].value}, fail:)`,user[prop] && !eval(`${Date.parse(user[prop])} ${filter[prop].operator} ${filter[prop].value}`));
+
+            } else if (prop === 'availabilityMin'){
+              console.log(`${user.name}: (${prop}: ${Date.parse(user[prop])}) ${filter[prop].operator} (input: ${filter[prop].value}, fail:)`,user[prop] && !eval(`${Date.parse(user[prop])} ${filter[prop].operator} ${filter[prop].value}`));
+            }
+
             if (prop === 'coordinates' && user['coordinates']) {
               let radiusInfo = isWithinMyRadius(filter[prop].value,user[prop],filter.radius.value);
               if (!radiusInfo[0]) {
                 return false;
               } else {
                 user['radius'] = radiusInfo[1];
+              }
+            } else if ((prop === 'availabilityMin' || prop === 'availabilityMax') ) {
+              if (user[prop] && !eval(`${Date.parse(user[prop])} ${filter[prop].operator} ${filter[prop].value}`)) {
+                return false;
               }
             } else if(prop !== 'radius' && prop.substring(0,3) !== 'rate' && user[prop] && !eval(`${user[prop]} ${filter[prop].operator} ${filter[prop].value}`)){
               return false;
